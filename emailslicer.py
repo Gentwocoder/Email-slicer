5b31d058d11b6b8104d1eba511e2b476fcf53b62
gentle <adetoyeseoyekanmi@gmail.com>
2022-06-22 18:53:07 +0100

    I created an email slicer with python

---
+++ emailslicer.py
@@ -0,0 +1,68 @@
+import tkinter as tk
+from tkinter import *
+
+window = tk.Tk()
+
+window.geometry("650x650")
+window.config(bg="#BE361A")
+window.resizable(width=False, height=False)
+window.title("Simple Email Slicer")
+
+
+def result():
+    try:
+        email = entry.get()
+        inp_email = email.strip()
+        username = inp_email[0:inp_email.index("@")]
+        domain = inp_email[inp_email.index("@")+1:]
+        text_box.delete("1.0", tk.END)
+        msg = "Email entered was: {}\nYour email username is {}\nAnd your email domain server is {}"
+        msg_formatted = msg.format(email, username, domain)
+        text_box.insert(tk.END, msg_formatted)
+        entry.delete(0, "end")
+    except:
+        text_box.delete("1.0", tk.END)
+        text_box.insert(tk.END, "ERROR!")
+
+
+def reset_all():
+    text_box.delete("1.0", tk.END)
+    entry.delete(0, "end")
+
+
+start = Label(text="Welcome to our Email Slicer", font=12, foreground="white", background="black")
+Info = Label(text="Enter your email ID click the done button!\n The application will extract your username and domain "
+                  "name.", foreground="white", background="#BE361A", font=10)
+emailId = Label(foreground="white", font=10, background="black", text="Enter Your Email Id: ")
+result_label = Label(font=10, foreground="white", background="black", text="The results are as follows: ")
+empty_label = Label(background="#BE361A")
+empty_label1 = Label(background="#BE361A")
+empty_label2 = Label(background="#BE361A")
+empty_label3 = Label(background="#BE361A")
+empty_label4 = Label(background="#BE361A")
+empty_label5 = Label(background="#BE361A")
+
+e1 = StringVar()
+entry = Entry(font=11, width=50, justify="center", textvariable=e1)
+
+btn = Button(text="Slice", font=11, command=result)
+reset = Button(text="Reset", font=11, command=reset_all)
+
+text_box = Text(height=5, width=40)
+
+empty_label.pack()
+start.pack()
+Info.pack()
+empty_label1.pack()
+emailId.pack()
+empty_label4.pack()
+entry.pack()
+empty_label2.pack()
+btn.pack()
+empty_label5.pack()
+reset.pack()
+empty_label3.pack()
+result_label.pack()
+text_box.pack()
+
+window.mainloop()
